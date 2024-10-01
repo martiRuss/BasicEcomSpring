5b66847d38cee3776c813f7974a574a67e62e6e8
martiRuss <151932744+martiRuss@users.noreply.github.com>
2024-10-01 22:44:37 +1000

    Add files via upload

---
+++ ecom-project/src/main/java/com/russel/ecom_project/service/ProductService.java
@@ -0,0 +1,49 @@
+package com.russel.ecom_project.service;
+
+import com.russel.ecom_project.model.Product;
+import com.russel.ecom_project.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo repo;
+
+    public List<Product> getAllProducts() {
+        return repo.findAll();
+    }
+
+    public Product getProduct(int id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageDate(imageFile.getBytes());
+
+        return repo.save(product);
+    }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageDate(imageFile.getBytes());
+        return repo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        repo.deleteById(id);
+    }
+
+    public List<Product> searchProducts(String keyword) {
+        return repo.searchProducts(keyword);
+    }
+}
---
+++ ecom-project/target/classes/data1.sql
@@ -0,0 +1,7 @@
+INSERT INTO product (name, desc, brand, price, category, release_Date, available, quantity)
+VALUES
+    ('Tata Nexon', 'A compact SUV with excellent safety features and performance.', 'Tata Motors', 750000, 'Cars', '2024-01-14T18:30:00.000+00:00', TRUE, 50),
+    ('Maruti Suzuki Swift', 'A popular hatchback known for its fuel efficiency and reliability.', 'Maruti Suzuki', 550000, 'Cars', '2024-01-31T18:30:00.000+00:00', TRUE, 100),
+    ('Hyundai Creta', 'A stylish SUV with advanced features and comfortable interior.', 'Hyundai', 950000, 'Cars', '2024-02-15T18:30:00.000+00:00', TRUE, 70),
+    ('Mahindra Thar', 'A rugged off-road SUV with a powerful engine and modern amenities.', 'Mahindra', 1250000, 'Cars', '2024-03-05T18:30:00.000+00:00', TRUE, 40),
+    ('Honda City', 'A premium sedan with a sleek design and advanced safety features.', 'Honda', 1100000, 'Cars', '2024-04-20T18:30:00.000+00:00', TRUE, 60);
